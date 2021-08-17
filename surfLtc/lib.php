6d0d5e955b51a364f1b438028d75b8c8c49e31b0
Abdur Rahman <abdurrahmanariyansyah@gmail.com>
2021-08-17 12:56:00 +0700

    add
    add

--- surfLtc/lib.php
+++ surfLtc/lib.php
@@ -1,6 +1,20 @@
 <?php
 
+$user_agent = "Mozilla/5.0 (Linux; Android 9; vivo 1902) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36";
+// $sessID = "fufo87hqcmmqqj0pph9r6na310";
+$cookie = "";
 $Capt = "";
+$ua = [
+  "Host: adltc.cc",
+  'user-agent: '.$user_agent, 
+  'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+  "cookie: "
+];
+
+$url = "https://adltc.cc/surf";
+
+$rURL = "https://adltc.cc/surf.php";
+
 $captcha = "g-recaptcha-response=".$Capt;
 
 ?>
