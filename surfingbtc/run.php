e48a6199c04108dee4d966e40d8ea036997963e7
Abdur Rahman <abdurrahmanariyansyah@gmail.com>
2021-08-18 10:44:08 +0700

    update run.php

--- surfingbtc/run.php
+++ surfingbtc/run.php
@@ -2,8 +2,9 @@
 error_reporting(0);
 system('clear');
 
-echo "Welcome to The Jungle";
+echo "Welcome to The Jungle \n";
 sleep(5);
+$rTimeWait = 60;
 while(true){
     include('xatoshi_data.php');
     $ch = curl_init();
@@ -16,6 +17,27 @@ while(true){
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
     $result = curl_exec($ch);
+    // print_r($result);
+    
+    // No adsPlease come anothger time
+    $nAds = explode('<div class="col-lg-12"><h3>', $result)[1];
+    $noAds = explode('.</div><br>', $nAds)[0];
+    $noAdsSh = explode('</h3><br>', $noAds);
+    
+    
+    
+    $aB = explode('Account Balance<br><b>', $result)[1];
+    $Bels = explode(' BTC</b>', $aB)[0];
+    
+    $aName = explode('<h4><b>', $result)[1];
+    $adsName = explode('</b></h4>', $aName)[0];
+    
+    $aPrice = explode('<h6>Price : <b>', $result)[1];
+    $adsPrice = explode('</b> BTC </h6>', $aPrice)[0];
+    
+    $aDrt = explode('Duration : <b>', $result)[1];
+    $adsDrt = explode('</b> second <a', $aDrt)[0];
+    
     $tk = explode('<a href="skip?token=', $result);
     $tk1 = explode('"', $tk[1]);
     $bc = explode('Account Balance<br><b>', $result);
@@ -36,12 +58,53 @@ while(true){
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
     $result = curl_exec($ch);
-    if($tk1[0] != ""){
+    
+    echo "\n\033[1;30mBelance : \033[1;33m {$Bels}\n"; sleep(1);
+    
+    
+
+  if($tk1[0] != ""){
+    echo "\n\033[1;30mAds : \033[1;33m {$adsName}\n"; sleep(1);
+    echo "\033[1;30mPrice claim :\033[1;33m{$adsPrice} BTC\n"; sleep(1);
+    echo "\033[1;30mDuration :\033[1;33m{$adsDrt} Second\n";sleep(1);
+    echo "\033[1;30mGet Token : ".$tk1[0]." \n";
+
     echo "\033[1;31m[\033[1;37m+\033[1;31m] \033[1;37mVISIT ADS ID \033[1;33m".$tk1[0]."\033[1;32m SUCCESS\n";
-    echo "\033[1;30m-----------------------------------------------------------\n";
+    
+    
+    $rLoad = $adsDrt + 1;
+    
+    for($i = $rLoad; $i > -1; $i--){
+      if($i % 2) {
+        echo "\033[1;30m=";
+        
+      }
+      sleep(1);
+    } // end for
+    echo '\n';
     }else{
-    echo "";
+      // <title id="title"></title>
+      if($Bels == NULL || $Bels = ""){
+        echo "Lakukan Pemindahan ulang \n";
+      }
+      
+      echo "\033[1;30m{$noAdsSh[0]} \033[1;33mReload \033[1;30m{$rTimeWait} Second\n";sleep(1);
+    echo "\033[1;30m{$noAdsSh[1]} \n";
+    echo "\033[1;31m";
+    for($i = $rTimeWait; $i > -1; $i--){
+      $m = floor($i / 60);
+      $d = $i % 60;
+      //if($i % 5) {
+      echo "                \r\r\r";
+        echo "xxxxxxxxxxxxxxx[{$m}:{$d}]xxxxxxxxxxx";
+       //  flush();
+        
+     //'' }
+      sleep(1);
     }
-    sleep(60);
+    $rTimeWait = $rTimeWait + 30;
     }
+    
+    }
+    
 ?>
