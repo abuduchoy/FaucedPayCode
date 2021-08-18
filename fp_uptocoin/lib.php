21981a19dce99321fe0159227c5be55b51553d85
Abdur Rahman <abdurrahmanariyansyah@gmail.com>
2021-08-19 06:30:21 +0700

    update lib.php

--- fp_uptocoin/lib.php
+++ fp_uptocoin/lib.php
@@ -1,5 +1,16 @@
 <?php
 
+// ISI SEMUA DATA DENGAN BENAR
+// JANGAN SKIP VIDEO SUPAYA GAK GAGAL
 
-echo "hello world";
-?>
+//user-agent
+$user = "Mozilla/5.0 (Linux; Android 10; Infinix X692) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36";
+
+//cookie
+$cookie = "a=niln10u17QpqAcsTsrmPZNTBGGfAlGvt; HstCfa4361570=1628652464174; HstCmu4361570=1628652464174; HstCnv4361570=1; HstCns4361570=1; c_ref_4361570=https%3A%2F%2Fwww.google.com%2F; __dtsu=6D0016284374359B319EBDFB636B2B47; _cc_id=98feee562799aef888c0e80e0f075c77; panoramaId_expiry=1629257270634; panoramaId=450a9f9382fb354b363edea13b7816d53938151383c6d9617a31def2226d86f2; __gads=ID=6c9f72ad395585cb-22fca495c3ca00c2:T=1628652490:RT=1628652490:S=ALNI_MblnMycgNGpDI0Ne6TBJ5dLkF1ihA; HstCla4361570=1628652496733; HstPn4361570=2; HstPt4361570=2; token_QpUJAAAAAAAAGu98Hdz1l_lcSZ2rY60Ajjk9U1c=BAYAYRNDsAFhE0PRgAGBAsAAIB6RxtGD-ZzD5hNkW9lOgjTAYg0bbgZzGpaXZlV-yeitwQBHMEUCIBgENtMClhveBY2i17hcohvn_v-KOPuowPB4Ti4OJqvdAiEAroH1bQ5n63c2RINhFCWzWDQ4enbs0dwKSIOGu6s9WJk; _popfired=1; _popfired_expires=Thu%2C%2012%20Aug%202021%2003%3A28%3A20%20GMT; lastOpenAt_=1628652500666";
+
+//timer-claim-dalam-satuan-detik
+$tmr = "60";
+
+//url-target-claim
+$webtarget = "http://uptocoin.tk/fp/faucet.php?address=TQWEaCKTVNzFSi7s29RamaiQou8VpgxNfF&currency=DOGE&key=66c0b0a94cad9b6b4ce0923f31e745b2";
