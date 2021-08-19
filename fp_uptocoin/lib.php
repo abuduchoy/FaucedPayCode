66e5361d2d0a5bfef3bad9884a0e91fd76f66418
Abdur Rahman <abdurrahmanariyansyah@gmail.com>
2021-08-19 17:43:43 +0700

    update lib.php

--- fp_uptocoin/lib.php
+++ fp_uptocoin/lib.php
@@ -1,16 +1,17 @@
 <?php
-
+// cd storage/downloads/code/uptocoin
+// abuduchoy.github.io/FaucedPayCode
 //user-agent
 $user = "Mozilla/5.0 (Linux; Android 10; Infinix X692) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36";
-$user_lite = "";
-$user_kiwi = "";
-$user_x = "";
+$user_lite = "Mozilla/5.0 (Linux; Android 10; Infinix X692) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.114 Mobile Safari/537.36";
+$user_kiwi = "Mozilla/5.0 (Linux; Android 10; Infinix X692) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.39 Mobile Safari/537.36";
+$user_x = "Mozilla/5.0 (Linux; Android 10; Infinix X692) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.114 Mobile Safari/537.36";
 
 //cookie
-$cookie = "a=niln10u17QpqAcsTsrmPZNTBGGfAlGvt; HstCfa4361570=1628652464174; HstCmu4361570=1628652464174; HstCnv4361570=1; HstCns4361570=1; c_ref_4361570=https%3A%2F%2Fwww.google.com%2F; __dtsu=6D0016284374359B319EBDFB636B2B47; _cc_id=98feee562799aef888c0e80e0f075c77; panoramaId_expiry=1629257270634; panoramaId=450a9f9382fb354b363edea13b7816d53938151383c6d9617a31def2226d86f2; __gads=ID=6c9f72ad395585cb-22fca495c3ca00c2:T=1628652490:RT=1628652490:S=ALNI_MblnMycgNGpDI0Ne6TBJ5dLkF1ihA; HstCla4361570=1628652496733; HstPn4361570=2; HstPt4361570=2; token_QpUJAAAAAAAAGu98Hdz1l_lcSZ2rY60Ajjk9U1c=BAYAYRNDsAFhE0PRgAGBAsAAIB6RxtGD-ZzD5hNkW9lOgjTAYg0bbgZzGpaXZlV-yeitwQBHMEUCIBgENtMClhveBY2i17hcohvn_v-KOPuowPB4Ti4OJqvdAiEAroH1bQ5n63c2RINhFCWzWDQ4enbs0dwKSIOGu6s9WJk; _popfired=1; _popfired_expires=Thu%2C%2012%20Aug%202021%2003%3A28%3A20%20GMT; lastOpenAt_=1628652500666";
+$cookie = "HstCfa4361570=1628652464174; HstCmu4361570=1628652464174; HstCnv4361570=1; HstCns4361570=1; c_ref_4361570=https%3A%2F%2Fwww.google.com%2F; __dtsu=6D0016284374359B319EBDFB636B2B47; _cc_id=98feee562799aef888c0e80e0f075c77; __gads=ID=6c9f72ad395585cb-22fca495c3ca00c2:T=1628652490:RT=1628652490:S=ALNI_MblnMycgNGpDI0Ne6TBJ5dLkF1ihA; HstCla4361570=1628652496733; HstPn4361570=2; HstPt4361570=2";
 
 // cookie referal
-$cookie_1 = "";
+$cookie_1 = "HstCfa4361570=1628652464174; HstCmu4361570=1628652464174; HstCnv4361570=1; HstCns4361570=1; c_ref_4361570=https%3A%2F%2Fwww.google.com%2F; __dtsu=6D0016284374359B319EBDFB636B2B47; _cc_id=98feee562799aef888c0e80e0f075c77; __gads=ID=6c9f72ad395585cb-22fca495c3ca00c2:T=1628652490:RT=1628652490:S=ALNI_MblnMycgNGpDI0Ne6TBJ5dLkF1ihA; HstCla4361570=1628652496733; HstPn4361570=2; HstPt4361570=2";
 
 $cookie_2 = "";
 
@@ -18,27 +19,41 @@ $cookie_2 = "";
 $tmr = "60";
 
 // walet
-$w = "D6Lh3ALK2Gx5QsanxxfcoE6P2TiHnCJ21a";
+// http://uptocoin.tk/fp/?r=TQWEaCKTVNzFSi7s29RamaiQou8VpgxNfF&rc=DOGE
+$w = "TQWEaCKTVNzFSi7s29RamaiQou8VpgxNfF";
 $key = "66c0b0a94cad9b6b4ce0923f31e745b2";
 
+// http://uptocoin.tk/fp/faucet.php?r=TQWEaCKTVNzFSi7s29RamaiQou8VpgxNfF&rc=DOGE&address=DF4fuDcUTakiiGUdt9xq5T7mbGv8CviRPq&currency=DOGE&key=0b34775ba375b79627051d521d1f315f
 $w_reff_1 = "DF4fuDcUTakiiGUdt9xq5T7mbGv8CviRPq";
 $key_reff_1 = "0b34775ba375b79627051d521d1f315f";
 
-$w_reff_2 = "";
-$key_reff_2 = "";
+// http://uptocoin.tk/fp/faucet.php?r=D6Lh3ALK2Gx5QsanxxfcoE6P2TiHnCJ21a&rc=DOGE&address=DGg978VFUeYgfWR4AT9kRpwasTsZJ1Yb39&currency=DOGE&key=409b29502a24e3d071b24baf0299973a
+$w_reff_2 = "DGg978VFUeYgfWR4AT9kRpwasTsZJ1Yb39";
+$key_reff_2 = "409b29502a24e3d071b24baf0299973a";
+
+
+$w_reff_3 = "DK241G5Lim45ujMq9FBCFYvQhYr48mVDq9";
+$key_reff_3 = "dea4c7f0e64b8b5361f66e3b44d23366";
+
+// http://uptocoin.tk/fp/faucet.php?r=D6Lh3ALK2Gx5QsanxxfcoE6P2TiHnCJ21a&rc=DOGE&address=DGGkjUJtPT7si32iHrqXJpbhV6CheGmQeA&currency=DOGE&key=f7d20d0a3191f7ea371fc70db19c6b84
+$w_reff_4 = "DGGkjUJtPT7si32iHrqXJpbhV6CheGmQeA";
+$key_reff_4 = "f7d20d0a3191f7ea371fc70db19c6b84";
+
 //url-target-claim
 $webtarget = "http://uptocoin.tk/fp/faucet.php?address={$w}&currency=DOGE&key={$key}";
 
-
 // url ref
-$url_reff_1 = "http://uptocoin.tk/fp/faucet.php?r={$w}=DOGE&address={$w_reff_1}&currency=DOGE&key={$key_reff_1}";
-$url_reff_2 = "http://uptocoin.tk/fp/faucet.php?r={$w}=DOGE&address={$w_reff_2}&currency=DOGE&key={$key_reff_2}";
+$url_reff_1 = "http://uptocoin.tk/fp/faucet.php?r={$w}&rc=DOGE&address={$w_reff_1}&currency=DOGE&key={$key_reff_1}";
+$url_reff_2 = "http://uptocoin.tk/fp/faucet.php?r={$w}&rc=DOGE&address={$w_reff_2}&currency=DOGE&key={$key_reff_2}";
+$url_reff_3 = "http://uptocoin.tk/fp/faucet.php?r={$w}&rc=DOGE&address={$w_reff_3}&currency=DOGE&key={$key_reff_3}";
+
+$url_reff_4 = "http://uptocoin.tk/fp/faucet.php?r={$w}&rc=DOGE&address={$w_reff_4}&currency=DOGE&key={$key_reff_4}";
 
 function cek($var){
   if($var == "") {
     $q = "Kosong";
   } else {
-    $q = "Siap"
+    $q = "Siap";
   }
 } // end funct
 
