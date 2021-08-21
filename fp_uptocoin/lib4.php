bb63cfdae6ce8d0a4e1a2685ae9cfe2ea7be1656
Abdur Rahman <abdurrahmanariyansyah@gmail.com>
2021-08-21 17:19:14 +0700

    a
    a

--- fp_uptocoin/lib4.php
+++ fp_uptocoin/lib4.php
@@ -1,5 +1,145 @@
 <?php
 /**
+DSYoXvTnNdQGv4StebVtzwqZ3yMdQAP7Rp
+#############
+DNa6cfDBx4Vryb1v5vXdmZcNZ5CTpRpT3D
+DNbS3bCXC4SCJyVo2oNR4jzb4XxWsGLUM9
+D6M7p1m5Ub9uPPLreqdwxvmwPLRVbZampy
+DJW5SKbuUkRMMuk3cmcrCQmCkni58beFBB
+DHq8Eci9zU1n8hNs9mdscUUREyWBdL8yGa
+
+DQU2CAN3Gj67NpLZWHpV3qyKMR3RaK3pyC
+D6oFgRN3gF2E2dX7o5GSjE4gsu5TWMht21
+DTwQTBn6PkFan5fQmqGT7PNrt3e9o6v1NB
+D9Y5oVoSwiwsmetw1SdhjwQ3NFXFQR6gtN
+DSNERNbN7pRKyA738trCxAcawu3wCPqiMM
+
+DNwumB6WgurC9vHVm6rNhBqwLqfXQtqs7o
+D6cxT53us22tD1UTfVP34e8xZ45qMNUHej
+DKoLDAxjcGPf6CbHPHdAGooX1CWsKUXBrU
+DFhAkQDUgGKvNF4k8gbt1aJyFWoEwTkaub
+DQnaZ76dCc7SDhgo7kATM5M9Qxm8z4ZVH4
+
+DTXgmLN8e6P2zvkVjLKMjJK8iRFcMhZ8VY
+DT3dMnFSqa1Z1RpyX4yDKSAj2niPL5Wrtm
+DDPErJwjA87S3LjUieDKh9CVcM8nvguWz8
+DTbfAnixDMNTJXAZJSR6xJakdgDdYYvWPW
+DBTTAyqtXa9EdipFHmfZJmj8aGcPsVAfY2
+
+##########################
+DNyjv2NBNLsuwVrBCNZmVesmQRrvZKwHAz
+DFcC7AdBNLavyWa8FURGdouHL3XxG2suq3
+DSGpYh5cWWE6D73sxG7dGozRwZ66QVNKN2
+  D8HebbVbBgbV3FPhwsFUcrBF8g4S1zUxdy
+DA6ZgUnCscQpbD5zxqvBv2vMcb4JChyvzb
 
 **/
+
+$reff = [
+0 => [
+ 'key'=> "",
+ 'w'=> "DSYoXvTnNdQGv4StebVtzwqZ3yMdQAP7Rp"
+],
+1 => [
+ 'key'=> "",
+ 'w'=> "DNa6cfDBx4Vryb1v5vXdmZcNZ5CTpRpT3D"
+],
+2 => [
+ 'key'=> "",
+ 'w'=> "DNbS3bCXC4SCJyVo2oNR4jzb4XxWsGLUM9"
+],
+3 => [
+ 'key'=> "",
+ 'w'=> "D6M7p1m5Ub9uPPLreqdwxvmwPLRVbZampy"
+],
+4 => [
+ 'key'=> "",
+ 'w'=> "DJW5SKbuUkRMMuk3cmcrCQmCkni58beFBB"
+],
+5 => [
+ 'key'=> "",
+ 'w'=> "DHq8Eci9zU1n8hNs9mdscUUREyWBdL8yGa"
+],
+6 => [
+ 'key'=> "",
+ 'w'=> "DSNERNbN7pRKyA738trCxAcawu3wCPqiMM"
+],
+7 => [
+ 'key'=> "",
+ 'w'=> "D9Y5oVoSwiwsmetw1SdhjwQ3NFXFQR6gtN"
+],
+8 => [
+ 'key'=> "",
+ 'w'=> "DTwQTBn6PkFan5fQmqGT7PNrt3e9o6v1NB"
+],
+9 => [
+ 'key'=> "",
+ 'w'=> "D6oFgRN3gF2E2dX7o5GSjE4gsu5TWMht21"
+],
+10 => [
+ 'key'=> "",
+ 'w'=> "DQU2CAN3Gj67NpLZWHpV3qyKMR3RaK3pyC"
+],
+11 => [
+ 'key'=> "",
+ 'w'=> "DNwumB6WgurC9vHVm6rNhBqwLqfXQtqs7o"
+],
+12 => [
+ 'key'=> "",
+ 'w'=> "D6cxT53us22tD1UTfVP34e8xZ45qMNUHej"
+],
+13 => [
+ 'key'=> "",
+ 'w'=> "DKoLDAxjcGPf6CbHPHdAGooX1CWsKUXBrU"
+],
+14 => [
+ 'key'=> "",
+ 'w'=> "DFhAkQDUgGKvNF4k8gbt1aJyFWoEwTkaub"
+],
+15 => [
+ 'key'=> "",
+ 'w'=> "DQnaZ76dCc7SDhgo7kATM5M9Qxm8z4ZVH4"
+],
+16 => [
+ 'key'=> "",
+ 'w'=> "DTXgmLN8e6P2zvkVjLKMjJK8iRFcMhZ8VY"
+],
+17 => [
+ 'key'=> "",
+ 'w'=> "DT3dMnFSqa1Z1RpyX4yDKSAj2niPL5Wrtm"
+],
+18 => [
+ 'key'=> "",
+ 'w'=> "DDPErJwjA87S3LjUieDKh9CVcM8nvguWz8"
+],
+19 => [
+ 'key'=> "",
+ 'w'=> "DTbfAnixDMNTJXAZJSR6xJakdgDdYYvWPW"
+],
+20 => [
+ 'key'=> "",
+ 'w'=> "DBTTAyqtXa9EdipFHmfZJmj8aGcPsVAfY2"
+],
+21 => [
+ 'key'=> "",
+ 'w'=> "DNyjv2NBNLsuwVrBCNZmVesmQRrvZKwHAz"
+],
+22 => [
+ 'key'=> "",
+ 'w'=> "DFcC7AdBNLavyWa8FURGdouHL3XxG2suq3"
+],
+23 => [
+ 'key'=> "",
+ 'w'=> "DSGpYh5cWWE6D73sxG7dGozRwZ66QVNKN2"
+],
+24 => [
+ 'key'=> "",
+ 'w'=> "D8HebbVbBgbV3FPhwsFUcrBF8g4S1zUxdy"
+],
+25 => [
+ 'key'=> "",
+ 'w'=> "DA6ZgUnCscQpbD5zxqvBv2vMcb4JChyvzb"
+]
+  ];
+
 ?>
