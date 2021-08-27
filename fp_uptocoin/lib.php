b73903f41da5798016c9046a8cf68efdda84901c
Abdur Rahman <abdurrahmanariyansyah@gmail.com>
2021-08-27 08:34:21 +0700

    update lib.php

--- fp_uptocoin/lib.php
+++ fp_uptocoin/lib.php
@@ -10,83 +10,43 @@ $user_x = "Mozilla/5.0 (Linux; Android 10; Infinix X692) AppleWebKit/537.36 (KHT
 //cookie
 $cookie = "HstCfa4361570=1628652464174; HstCmu4361570=1628652464174; HstCnv4361570=1; HstCns4361570=1; c_ref_4361570=https%3A%2F%2Fwww.google.com%2F; __dtsu=6D0016284374359B319EBDFB636B2B47; _cc_id=98feee562799aef888c0e80e0f075c77; __gads=ID=6c9f72ad395585cb-22fca495c3ca00c2:T=1628652490:RT=1628652490:S=ALNI_MblnMycgNGpDI0Ne6TBJ5dLkF1ihA; HstCla4361570=1628652496733; HstPn4361570=2; HstPt4361570=2";
 
-//timer-claim-dalam-satuan-detik
-$tmr = "60";
+// cookie referal
+$cookie_1 = "HstCfa4361570=1628652464174; HstCmu4361570=1628652464174; HstCnv4361570=1; HstCns4361570=1; c_ref_4361570=https%3A%2F%2Fwww.google.com%2F; __dtsu=6D0016284374359B319EBDFB636B2B47; _cc_id=98feee562799aef888c0e80e0f075c77; __gads=ID=6c9f72ad395585cb-22fca495c3ca00c2:T=1628652490:RT=1628652490:S=ALNI_MblnMycgNGpDI0Ne6TBJ5dLkF1ihA; HstCla4361570=1628652496733; HstPn4361570=2; HstPt4361570=2";
 
-// Master
 $w = "TQWEaCKTVNzFSi7s29RamaiQou8VpgxNfF";
 $key = "66c0b0a94cad9b6b4ce0923f31e745b2";
 
+
+
 $webtarget = "http://uptocoin.tk/fp/faucet.php?address={$w}&currency=DOGE&key={$key}";
 
-// array ...
+//timer-claim-dalam-satuan-detik
+$tmr = "60";
 
 $reff = [
       0 => [
-     'key'=> "0b3f7c475ea49a8890097ae164564ab4",
-     'w'=> "DEywZMmog3YDWjehXxRbW8PtCNhWu8Wgaa"
+'key'=> "0b34775ba375b79627051d521d1f315f",
+'w'=> "DF4fuDcUTakiiGUdt9xq5T7mbGv8CviRPq"
     ],
     1 => [
-     'key'=> "eaf9d70061dc9b7ddd3a5abf3a93fabc",
-     'w'=> "D7Ng7vQs4oTmB5SR3GZHkhptHRLjmVACFQ"
+'key'=> "409b29502a24e3d071b24baf0299973a",
+'w'=> "DGg978VFUeYgfWR4AT9kRpwasTsZJ1Yb39"
     ],
-    2 => [
-     'key'=> "25065961f8f7e918b5f16bf6e15a855d",
-     'w'=> "DRSJsUTs8qKaX3CcbPhWRaErJR5ZWfsMAH"
+    2  => [
+'key'=> "dea4c7f0e64b8b5361f66e3b44d23366",
+'w'=> "DK241G5Lim45ujMq9FBCFYvQhYr48mVDq9"
     ],
-    3 => [
-     'key'=> "",
-     'w'=> "DCEKx4btFMaGxGAhvaNMgwiUZgUZNQ1waF"
-    ],
-    4 => [
-     'key'=> "",
-     'w'=> "D8P7iFHhprPCErPrNVL7hrjiBMFfLgv55f"
-    ],
-    5 => [
-     'key'=> "",
-     'w'=> "DC8U2kPjE4rRSy2PEkbCFUhTuMPqj2MAY3"
-    ],
-    6 => [
-     'key'=> "",
-     'w'=> "DG5DETGJxofJc34Mz2J1V35AV8CCfcD1Xa"
-    ],
-    7 => [
-     'key'=> "",
-     'w'=> "DDfoYioenYM7wyYGWr5UkjJM9KPZrig93q"
-    ],
-    8 => [
-     'key'=> "",
-     'w'=> "D5Qs9bt2HrtBs4fGbKhaC4fCusdibadgBo"
-    ],
-    9 => [
-     'key'=> "",
-     'w'=> "D6mnmiYt4kKVa3yyHfz2D2N2mLDycwJaAa"
-    ],
-    10 => [
-     'key'=> "",
-     'w'=> "DS83FpwpHYcVjFWQ4JRWWpU4PxY8SseA9T"
-    ],
-    11 => [
-     'key'=> "",
-     'w'=> "DEXw5DPK2mxFeePxzoDHdrZpDk13oDBbNX"
-    ],
-    12 => [
-     'key'=> "",
-     'w'=> "DPNPCzSbfPPEVeanoedzAHHcM4V4Bvrm2P"
+    3  => [
+'key'=> "f7d20d0a3191f7ea371fc70db19c6b84",
+'w'=> "DGGkjUJtPT7si32iHrqXJpbhV6CheGmQeA"
     ]
+    
   ];
 
-// URL array
+
+
 for($i = 0; $i < count($reff); $i++){
 $urlReff[$i] = "http://uptocoin.tk/fp/faucet.php?r={$w}&rc=DOGE&address={$reff[$i]['w']}&currency=DOGE&key={$reff[$i]['key']}";
 }
-
-function cek($var){
-  if($var == "") {
-    $q = "Kosong";
-  } else {
-    $q = "Siap";
-  }
-} // end funct
 
 ?>
