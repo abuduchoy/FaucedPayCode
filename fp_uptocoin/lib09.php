b18252d965ff2d2ab46ee7c04405c28d635e97ab
Abdur Rahman <abdurrahmanariyansyah@gmail.com>
2021-08-30 15:06:02 +0700

    Upload
    Upload

---
+++ fp_uptocoin/lib09.php
@@ -0,0 +1,110 @@
+<?php
+// batas_26#: Modul_004;
+$reff = [
+0 => [
+  'key' =>"",
+  'w' =>"DNA7S2jLNctNrVCC3CTvwKdNrcBHALWCM5"
+ ],
+1 => [
+  'key' =>"",
+  'w' =>"DH6DTsv3DhWNJ22ZFSKxFNS7wKLdw3c7R7"
+ ],
+2 => [
+  'key' =>"",
+  'w' =>"DKh8ZKK6AkWaUj2VUbUYj99rnLVtNnjRTS"
+ ],
+3 => [
+  'key' =>"",
+  'w' =>"DNivcsQQyXZ9aofeqz5h1wwxayvJEZxNxj"
+ ],
+4 => [
+  'key' =>"",
+  'w' =>"D9EUPDhGUMhyv5j73Nx1Cm5xmz5AgQdB1z"
+ ],
+5 => [
+  'key' =>"",
+  'w' =>"D6Jcicc7YLV9UwyzSzerw9m8h18qAPhibT"
+ ],
+6 => [
+  'key' =>"",
+  'w' =>"DDKJTPXPcec1ontsQJFewjx56bwRDGXASK"
+ ],
+7 => [
+  'key' =>"",
+  'w' =>"DEx7sZCWBVTUbnva3XJ1NcrF4hUPV6zztM"
+ ],
+8 => [
+  'key' =>"",
+  'w' =>"DKAjNo5gLcCdnffpAXoiA1wmwHh22UzTCy"
+ ],
+9 => [
+  'key' =>"",
+  'w' =>"DUCsVkGfYgnfZqSQGJrAUo5MZTiiv1zo8q"
+ ],
+10 => [
+  'key' =>"",
+  'w' =>"DTKagSCZoUHB43icq2fyrxUHq98GyCzt1c"
+ ],
+11 => [
+  'key' =>"",
+  'w' =>"DKDJWkm85YJe5zUoZ1hjfJ7w1xBHPLfbDX"
+ ],
+12 => [
+  'key' =>"",
+  'w' =>"DDqPbUbXDiBdYZowkNzWMk8HT7u4uCaTFj"
+ ],
+13 => [
+  'key' =>"",
+  'w' =>"DKJDTW1qLovrXtxA9CL7G3A9Wh2BWnodST"
+ ],
+14 => [
+  'key' =>"",
+  'w' =>"DJouVaBD5TJM1G9nrsMPdstNAJbwyQXNCA"
+ ],
+15 => [
+  'key' =>"",
+  'w' =>"DGiSZFEagvA1jAqbGGSjVf71D1FxR4S4kp"
+ ],
+16 => [
+  'key' =>"",
+  'w' =>"DHMn9nEjvMgvSz8TPwUZiKwqpQ36rn6cdJ"
+ ],
+17 => [
+  'key' =>"",
+  'w' =>"DQ3po3UpVRmLS6oBjPZojEJKuvKXLRqLbw"
+ ],
+18 => [
+  'key' =>"",
+  'w' =>"DKwxdfHEfxHMExcSxGAoQS9Z95UTYmyXeJ"
+ ],
+19 => [
+  'key' =>"",
+  'w' =>"DCddRedLubHtbEPbSZWzxnqNqjFtZXnFmM"
+ ],
+20 => [
+  'key' =>"",
+  'w' =>"D9D9aaRNj3UgDx8NVydgm2AGe8BxpDAEsK"
+ ],
+21 => [
+  'key' =>"",
+  'w' =>"DKirPDgomLZEcCWEs99sdidpgNcp8Xiv2o"
+ ],
+22 => [
+  'key' =>"",
+  'w' =>"DHs9ufTLAGZMw97eNDudnMAP1XEXbTQugU"
+ ],
+23 => [
+  'key' =>"",
+  'w' =>"DFpTvmpadQwL9yqWbZMa4ExWX2e9VJSDuh"
+ ],
+24 => [
+  'key' =>"",
+  'w' =>"DKkJ9UXWf23bkGtv3DUb3ht88QJMfvYBDi"
+ ],
+25 => [
+  'key' =>"",
+  'w' =>"DEFSYHRBTeBWAs4itqPwyrq9DXfgiAHamq"
+ ],
+];
+
+?>
